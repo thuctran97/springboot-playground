357e3e207912eb4094e55497104866d2e5bf6a18
ttran <ttran@absolute.com>
2021-05-19 20:53:42 +0700

    add handle upload 2

--- src/main/java/com/springboot/controller/FileUploadController.java
+++ src/main/java/com/springboot/controller/FileUploadController.java
@@ -8,10 +8,14 @@ package com.springboot.controller;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.springboot.service.FileService;
 
+import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.fileupload.util.Streams;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -28,6 +32,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -36,7 +43,11 @@ public class FileUploadController {
     @Autowired
     FileService fileService;
 
-    @RequestMapping(value = "/upload2", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public final int MAX_MERMORY_sIZE = 1024 * 1024 * 3;
+    public final int MAX_FILE_sIZE = 1024 * 1024 * 500;
+    public final int MAX_REQUEST_SIZE = 1024 * 1024 * 1000;
+
+    @RequestMapping(value = "/normal-upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Object> uploadFileTest(@RequestParam("file") MultipartFile file) throws IOException {
         File convertFile = new File("C:\\Users\\ttran\\Downloads\\temp\\" + file.getOriginalFilename());
         convertFile.createNewFile();
@@ -48,21 +59,20 @@ public class FileUploadController {
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String handleUpload(HttpServletRequest request) {
-//        ObjectMetadata metadata = new ObjectMetadata();
 //        String fileName = fileService.getFileName(request);
 //        System.out.println("fileName: "+fileName);
         ServletFileUpload upload = new ServletFileUpload();
         try {
             FileItemIterator iterStream =  upload.getItemIterator(request);
             int count = 0;
-            System.out.println("hasNext: "+iterStream.hasNext());
             while (iterStream.hasNext()) {
-                System.out.println("iterStream: " + count++);
+                System.out.println("count: " + count++);
                 FileItemStream item = iterStream.next();
                 String name = item.getFieldName();
                 InputStream stream = item.openStream();
                 if (!item.isFormField()) {
-                    fileService.upload(stream, "abc", new ObjectMetadata());
+                    fileService.upload(stream, "temp", new ObjectMetadata());
+                    stream.close();
                 } else {
                     String formFieldValue = Streams.asString(stream);
                     System.out.println("formFieldValue: "+formFieldValue);
@@ -72,6 +82,37 @@ public class FileUploadController {
             e.printStackTrace();
         }
         return "success";
+    }
+
+    @RequestMapping(value = "/upload2", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String handleUpload2(HttpServletRequest request) throws FileUploadException, IOException {
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+        System.out.println("isMultipart: "+ isMultipart);
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        factory.setRepository(
+                new File(System.getProperty("java.io.tmpdir")));
+        factory.setSizeThreshold(MAX_MERMORY_sIZE);
+        factory.setFileCleaningTracker(null);
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        upload.setFileSizeMax(MAX_FILE_sIZE);
+        upload.setSizeMax(MAX_REQUEST_SIZE);
+        List items = upload.parseRequest(request);
+        System.out.println("list items: "+ items.size());
+        Iterator iter = items.iterator();
+        int count = 0;
+        while (iter.hasNext()) {
+            FileItem item = (FileItem) iter.next();
+            System.out.println("count: " + count++);
+            if (!item.isFormField()) {
+                try (
+                        InputStream uploadedStream = item.getInputStream();
+                        OutputStream out = new FileOutputStream("file.mov");) {
+
+                    IOUtils.copy(uploadedStream, out);
+                }
+            }
+        }
+        return "success!";
     }
 
 }
